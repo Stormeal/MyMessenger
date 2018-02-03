e2afd46327016210b2b426d8ecd86a8b0d368cf9
Alex Storm <alex6614@edu.easj.dk>
2018-02-03 15:06:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alex/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,25 @@
+package com.example.alex.mymessenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    //Call onSendMessage() when the button is clicked
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMesageActivity.class);
+        intent.putExtra(ReceiveMesageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
